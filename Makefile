395b2c1d1e137a901601ce4efea2f209633adce9
David de Regt <deregtd@outlook.com>
2023-10-27 12:34:34 -0400

    feat: Moving in Units library, adding renovate and linting, updating tools

--- Makefile
+++ Makefile
@@ -1,3 +1,8 @@
 .PHONY: test
 test:
 	@go test ./... -coverprofile=c.out
+
+.PHONY: lint
+lint:
+	@golangci-lint run
+	@go run github.com/getoutreach/lintroller/cmd/lintroller@v1.17.0 -config lintroller.yaml ./...
---
+++ pkg/units/temperature.go
@@ -0,0 +1,62 @@
+package units
+
+import "fmt"
+
+// TemperatureUnit is an enum for all temperature unit types
+type TemperatureUnit int
+
+// The different TemperatureUnits
+const (
+	// Kelvin is Kelvin
+	Kelvin TemperatureUnit = iota
+	// Farenheit is Farenheit
+	Farenheit TemperatureUnit = iota
+	// Celsius is Celsius
+	Celsius TemperatureUnit = iota
+)
+
+// Temperature is a generic Unit structure that represents temperatures
+type Temperature struct {
+	Value    float32
+	Unit     TemperatureUnit
+	UnitType string
+}
+
+// NewTemperature creates a temperature unit of a given type and value
+func NewTemperature(u TemperatureUnit, value float32) Temperature {
+	return Temperature{
+		Unit:     u,
+		Value:    value,
+		UnitType: "temp",
+	}
+}
+
+// Convert converts the unit+value into a new unit type, returning a new unit value of the requested type.
+func (p Temperature) Convert(newUnit TemperatureUnit) Temperature {
+	// Shortcut (ez optimization)
+	if p.Unit == newUnit {
+		return p
+	}
+
+	var inKelvin float32
+	switch p.Unit {
+	case Kelvin:
+		inKelvin = p.Value
+	case Farenheit:
+		inKelvin = (p.Value + 459.67) * (5.0 / 9.0)
+	case Celsius:
+		inKelvin = p.Value + 273.15
+	default:
+		panic(fmt.Sprintf("Unknown old temperature unit %+v", p.Unit))
+	}
+	switch newUnit {
+	case Kelvin:
+		return NewTemperature(newUnit, inKelvin)
+	case Farenheit:
+		return NewTemperature(newUnit, inKelvin*(9.0/5.0)-459.67)
+	case Celsius:
+		return NewTemperature(newUnit, inKelvin-273.15)
+	default:
+		panic(fmt.Sprintf("Unknown new temperature unit %+v", newUnit))
+	}
+}
